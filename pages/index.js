ea8c2243429c1890476484c9993e9a404f14ed39
Nicolas Riquelme <nrriquelme@gmail.com>
2021-02-27 22:27:07 -0300

    Listening and loading files. Happy path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,65 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React, { useReducer } from 'react';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOADING':
+      const { payload: currentProgress } = action;
+      return {
+        ...state,
+        progress: currentProgress,
+      };
+    case 'FIRST_LOADING':
+      const { payload: size } = action;
+      return {
+        ...state,
+        size,
+      };
+    default:
+      break;
+  }
+};
 
 export default function Home() {
+  const initState = {
+    progress: 0,
+  };
+
+  const [state, dispatcher] = useReducer(reducer, initState);
+
+  const handleChange = (evt) => {
+    const {
+      target: { files },
+    } = evt;
+    console.log(files);
+
+    dispatcher({ type: 'FIRST_LOADING', payload: files[0].size });
+
+    const reader = new FileReader();
+
+    reader.addEventListener('progress', (e) => {
+      console.log(`${e.type} - ${e.loaded}`);
+      dispatcher({
+        type: 'LOADING',
+        payload: parseInt(e.loaded, 10),
+      });
+    });
+    reader.addEventListener('loadend', (e) => {
+      console.log(`${e.type} - ${e.loaded}`);
+    });
+
+    reader.addEventListener('load', (e) => {
+      console.log(`${e.type} - ${e.loaded}`);
+    });
+
+    for (let i = 0; i < files.length; i++) {
+      reader.readAsText(files[i]);
+    }
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+  };
+
   return (
     <div className="flex flex-col h-screen ml-8 mr-8">
       <div className="h-screen">
@@ -12,18 +70,32 @@ export default function Home() {
         <div className="flex flex-col h-1/5 w-full border-2 p-4 rounded-lg border-purple-400">
           <div className="flex flex-col h-full">
             <form
+              onSubmit={handleSubmit}
               action=""
               className="flex flex-col justify-center items-center h-full"
             >
-              <input type="file" />
-              <button>Upload</button>
+              <div className="flex flex-row w-full h-3/5 justify-center items-center">
+                <input
+                  type="file"
+                  onChange={handleChange}
+                  multiple="multiple"
+                />
+              </div>
+              <div className="flex flex-row w-full h-3/6 justify-center items-end">
+                <button
+                  type="submit"
+                  className="border-2 pl-4 pr-4 pt-1 pb-1 rounded-lg border-blue-400"
+                >
+                  Upload
+                </button>
+              </div>
             </form>
           </div>
         </div>
 
         <div>
           <div>
-            <h2>Files</h2>
+            <h2>Files Loaded</h2>
           </div>
         </div>
       </div>
